19550201026be72e920c0272b10fb8102174cfd2
John-Anthony <jharwerth@udallas.edu>
2022-06-22 16:40:27 -0500

    additions after office hours

--- app.js
+++ app.js
@@ -1,57 +1,54 @@
-const gameArea = document.getElementById("board");
-
 const gameState = {
-    players: ["X", "O"],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ],
-    activePlayer: 0,
-  }
+  players: ["X", "O"],
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  activePlayer: 0
+};
 
-gameArea.addEventListener("click", boardClick(clickEvent));
+const gameArea = document.getElementById("board");
+gameArea.addEventListener("click", boardClick);
 
-function boardClick(clickEvent) {
-    updateBoard()
-    displayBoard()
-    let win = checkForWin()
-    if (win) {
-        updateWin()
-    }
-    else {
-        changeTurn()
-    }
+function boardClick(event) {
+//   updateBoard();
+//   displayBoard();
+//   let win = checkForWin();
+//   if (win) {
+//     updateWin();
+//   } else {
+//     changeTurn();
+//   }
 }
 
 // save the play in memory
-function updateBoard(clickEvent) {
-    let newBoard = []
-    if (this.board)
-
-
-    return newBoard
-}
+function updateBoard(event) {}
 
 // use memory to update user facing display (html)
-function displayBoard(clickEvent) {}
+function displayBoard(event) {
+  event.target.innerText = "o";
+}
 
 // see if any win condition is fulfilled, similar to sudoku in checking the output of rows, columns, and diagonals
 function checkForWin() {
-    includesThree()
+  includesThree();
 }
 
 // helps checkForWin, looks for three in a row
-function includesThree() {}
+function includesThree() {
 
-function updateWin() {}
+}
 
-// 
+function updateWin() {
+
+}
+
+//switch from player to player
 function changeTurn() {
-    if (activePlayer === 1) {
-        gameState.activePlayer = 0;
-    }
-    else {
-        gameState.activePlayer = 1;
-    }
+  if (activePlayer === 1) {
+    gameState.activePlayer = 0;
+  } else {
+    gameState.activePlayer = 1;
+  }
 }
