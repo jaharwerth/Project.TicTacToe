3f6ad0e0e7aef816d30cc6f8a058543dd681c98a
John-Anthony <jharwerth@udallas.edu>
2022-06-26 10:08:28 -0500

    Saturday progress

--- app.js
+++ app.js
@@ -1,59 +1,131 @@
 const gameState = {
-  players: ["X", "O"],
+  players: ["x", "o"],
   board: [
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ],
-  activePlayer: 0
+  activePlayer: 0,
+  endingMessage: ["Congratulations!", "Draw"]
 };
 
-const gameArea = document.getElementById("board");
-gameArea.addEventListener("click", boardClick);
-
-function boardClick(event) {
-//   updateBoard();
-  displayBoard(event);
-//   let win = checkForWin();
-//   if (win) {
-//     updateWin();
-//   } else {
-//     changeTurn();
-//   }
-}
-
-// save the play in memory
-function updateBoard(event) {}
-
-// use memory to update user facing display (html)
-function displayBoard(event) {
-  event.target.innerText = "o";
-}
-
-// see if any win condition is fulfilled, similar to sudoku in checking the output of rows, columns, and diagonals
-function checkForWin() {
-  includesThree();
-}
-
-// helps checkForWin, looks for three in a row
-function includesThree() {
-
-}
-
-function updateWin() {
-
-}
-
-//switch from player to player
+// switch from player to player
 function changeTurn() {
-  if (activePlayer === 1) {
-    gameState.activePlayer = 0;
-  } else {
+  if (gameState.activePlayer === 0) {
     gameState.activePlayer = 1;
+  } else {
+    gameState.activePlayer = 0;
   }
 }
 
-// google: "how to put and remove a class in javascript" then watch first video
-// use element.classList.add to add a class
-// use element.classList.remove to remove a class
-// HTML forms: used to collect user input
+// saves the play in memory
+function updateGameState(event) {
+  if (!event.target.innerHTML) {
+    event.target.innerHTML = gameState.players[gameState.activePlayer]
+    changeTurn()
+  }
+}
+
+// let x1 = [
+//   ["x", "o", "x"],
+//   ["x", "o", "o"],
+//   ["o", "x", "x"],
+// ] 
+
+function getRow(board, rowIndex) {
+  let rowString = "";
+  let row = board[rowIndex];
+  for (i = 0; i < row.length; i++) {
+    rowString += row[i];
+  }
+  return rowString;
+}
+// console.log(getRow(x1, 0));
+
+function getColumn(board, columnIndex) {
+  let columnArray = [];
+  for (let i = 0; i < board.length; i++) {
+    let column = board[i][columnIndex];
+    columnArray += column;
+    }
+  return columnArray;
+}
+// console.log(getColumn(x1, 0))
+
+function getDiagonalLeft(board) {
+  let DiagonalLeftArray = []
+  DiagonalLeftArray += board[0][0] + board[1][1] + board[2][2]
+  return DiagonalLeftArray
+}
+// console.log(getDiagonalLeft(x1))
+
+function getDiagonalRight(board) {
+  let DiagonalRightArray = []
+  DiagonalRightArray += board[0][2] + board[1][1] + board[2][0]
+  return DiagonalRightArray
+}
+// console.log(getDiagonalRight(x1))
+
+function includesThree(array) {
+  let returnVal = true;
+  if (array === "xxx") {}
+  else if (array === "ooo") {}
+  else {
+    return false
+  }
+  return returnVal;
+}
+// console.log(includesThree(getRow(x1, 0)))
+
+function checkForWin(board) {
+  let returnVal = true;
+  for (let i = 0; i < board.length; i++) {
+    let rowIndex = i;
+    if (includesThree(getRow(board, rowIndex)) === true) {
+      return returnVal
+    }
+  }
+  for (let i = 0; i < board.length; i++) {
+    let columnIndex = i;
+    if (includesThree(getColumn(board, columnIndex)) === true) {
+      return returnVal
+    }
+  }
+  if (includesThree(getDiagonalLeft(board)) === true) {
+    return returnVal
+  }
+  else if (includesThree(getDiagonalRight(board)) === true) {
+    return returnVal
+  }
+  else {
+    return false;
+  }
+}
+// console.log(checkForWin(x1))
+
+function congratulations(event) {
+  if (checkForWin(gameState.board) === true) {
+    event.target.innerHTML = gameState.endingMessage[0]
+  }
+}
+
+function draw(event) {}
+
+function emptyBoardandPlayers() {}
+
+function boardClick(event) {
+  updateGameState(event)
+  let win = checkForWin(gameState.board)
+  if (win) {
+    congratulations()
+  }
+  // else {
+    // change activePlayer
+    // changeTurn()
+  // }
+}
+
+const gameArea = document.getElementById("board");
+gameArea.addEventListener("click", boardClick); 
+// const ending = document.getElementById("endingMessage");
+// ending.addEventListener("show", congratulations)
